7668c80242dccd52a96e3cc0295b6c61546bf23f
Vaibhav Bhadoriya <Vbhadoriya78@gmail.com>
2026-07-14 23:58:04 +0530

    Added Insertion sort

---
+++ Intro to python/CS_09_InsertionSort.py
@@ -0,0 +1,11 @@
+## Insertion Sort in Python
+aList = [15,6,13,22,3,52,2]
+print("Original List : ",aList)
+for i in range(1, len(aList)):
+    key = aList[i]
+    j = i-1
+    while j >=0 and key < aList[j] :
+            aList[j + 1] = aList[j]
+            j -= 1
+    aList[j + 1] = key
+print("List after Insertion Sort : ",aList)
