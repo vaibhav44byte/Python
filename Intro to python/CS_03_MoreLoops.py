e254ada1f1a307b39410d23e560c4dc606aab9fb
Vaibhav Bhadoriya <Vbhadoriya78@gmail.com>
2026-08-05 22:47:07 +0530

    Added while loop in python

--- Intro to python/CS_03_MoreLoops.py
+++ Intro to python/CS_03_MoreLoops.py
@@ -5,3 +5,9 @@ for a in range(1,4):
         print(a)
     else :
         print("The number is not divisible by 8")
+
+a = int(input("Enter a number : "))
+while(a>0):
+    print("The number is ",end="")
+    print(a)
+    a = a-1
